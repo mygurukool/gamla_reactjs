f1f88a183a74e198debf56c0c0bb9eecd16fbfde
Abhijeet Kumar <63132585+Abhijeet1520@users.noreply.github.com>
2021-12-13 18:25:17 +0530

    Change DATEFORMAT to "DD/MM/YYYY HH:mm:ss"
    - user will be also able to input time in the field

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const BASEURL = "";
 
 export const CURRENCY = "₮ ";
-export const DATEFORMAT = "DD/MM/YYYY";
+export const DATEFORMAT = "DD/MM/YYYY HH:mm:ss";
 
 export const SEARCHPARAMS = ["amount", "duration", "currency", "fundName"];
